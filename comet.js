ab8cd92098742ecae55b5c5ee83405b0e065c44b
Tikhon Jelvis <tikhon@berkeley.edu>
2011-04-17 15:48:05 -0700

    Started work on a comet server. What fun!

---
+++ comet.js
@@ -0,0 +1,33 @@
+// A simple node server that deals with comet connections. What fun!
+
+// Required componenets:
+var http = require("http");
+
+// Active connections:
+var connection = [];
+
+function update() {
+    if (connections.length) {
+        connection.forEach(function (c) {
+            console.log(JSON.stringify(c));
+            c.sendBody("oi\n");
+        });
+    }
+
+    setTimeout(update, 1000);
+}
+
+setTimeout(update, 1000);
+
+http.createServer(function (request, response) {
+    if (request.url == "/clock") {
+        request.connection.setTimeout(0);
+        response.writeHeader(200, {"Content-type" : "text/plain"});
+        connections.push(response);
+    } else {
+        response.writeHeader(404, {"Content-type" : "text/plain"});
+        response.end("Not found!");
+    }
+}).listen("8125");
+
+console.log("Running comet server on 127.0.0.1:8125");
