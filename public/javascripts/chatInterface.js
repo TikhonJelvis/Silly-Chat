ed96d6a29247ac8efc0e61fcb3baddfcb726f288
Tikhon Jelvis <tikhon@berkeley.edu>
2011-04-21 01:11:52 -0700

    I now have a script for the connection model and one for the interface. Fun times indeed.

---
+++ public/javascripts/chatInterface.js
@@ -0,0 +1,57 @@
+function ChatInterface(url, username) {
+    // Connection:
+    var connection = new ChatConnection(url, username);
+    
+    // Elements:
+    var element = $("<div>").addClass("chat"),
+        controls = $("<div>").addClass("controls"),
+        input = $("<input>").attr("type", "text"),
+        button = $("<button>").html("Go"),
+        messages = $("<div>");
+
+    input.submit(send);
+    button.click(send);
+
+    contols.append(input).append(button);
+
+    element.append(controls);
+    element.append(messages);
+
+    connection.observe(function (event) {
+        var messages = event.messages;
+        
+        for (var i = 0; i < messages.length; i++) {
+            addMessage(messages[i].message, messages[i].username);
+        }
+    });
+
+    /* Sends the message currently entered, clearing the input. */
+    function send() {
+        connection.sendMessage({message : input.val(), username : username});
+        input.val("");
+    }
+
+    /* Adds the given message to the messages div. */
+    function addMessage(text, username) {
+        var messageDiv = $("<div>").addClass("message"),
+            usernameDiv = $("<div>").addClass("username"),
+            contentDiv = $("<div>").addClass("messageText");
+
+        contentDiv.append(text);
+        usernameDiv.append(username);
+
+        messageDiv.append(usernameDiv).append(contentDiv);
+
+        messages.append(messageDiv);
+    }
+
+    /* Returns the parent div of the chat interface. */
+    this.getElement = function () {
+        return element;
+    };
+}
+
+$(function () {
+    var chat = new ChatInterface("chat", "Bob");
+    $("body").append(chat.getElement());
+});
